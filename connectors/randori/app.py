bb2dbf32c02723d323d48043463878acad3a9531
Omkar Gurav <omkargurav@Omkars-MacBook-Pro-2.local>
2022-11-09 15:42:41 -0400

    Added config file and clean up

--- connectors/randori/app.py
+++ connectors/randori/app.py
@@ -19,8 +19,6 @@ class App(BaseApp):
     def __init__(self):
         super().__init__('This script is used for pushing asset data to CP4S CAR ingestion microservice')
         # Parameters need to connect data source
-        self.parser.add_argument('-server', dest='server', default=os.getenv('CONFIGURATION_AUTH_TOKEN', "app.randori.io"),
-                                 type=str, required=False, help='Authentication token for the Okta data source')
         self.parser.add_argument('-access_token', dest='access_token', default=os.getenv('CONFIGURATION_AUTH_TOKEN', None),
                                  type=str, required=False, help='Authentication token for the Okta data source')
 
