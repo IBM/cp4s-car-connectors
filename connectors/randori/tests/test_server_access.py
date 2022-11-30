4b4bded4b8a1ce19e5441f96540008cccb940c40
Jessica Du <jingqiu@ca.ibm.com>
2022-11-30 16:04:03 -0400

    add test for test_connection

--- connectors/randori/tests/test_server_access.py
+++ connectors/randori/tests/test_server_access.py
@@ -6,6 +6,17 @@ from tests.test_utils import full_import_initialization, get_response, RandoriMo
 class TestAssetServer(unittest.TestCase):
     """Unit test for server access functions"""
 
+    @patch('randori_api.api.default_api.DefaultApi.get_hostname')
+    def test_connection(self, mock_res):
+        """Unit test for test_connection"""
+
+        mock_api_return_value = get_response('hostnames.json', True)
+        mock_hostnames_res = RandoriMockResponse(200, mock_api_return_value)
+        mock_res.return_value = mock_hostnames_res
+
+        actual_response = context().asset_server.test_connection()
+        assert actual_response is 0
+
     @patch('connector.server_access.AssetServer.get_detections_for_target')
     def test_get_detections_for_target(self, mock_detections_for_target):
         """Unit test for get_collection"""
