5fdc9e247d64dc822b79d16a9964030cd11b550d
Morgan Nolan <morgan-nolan@ibm.com>
2025-04-04 12:34:16 +0100

    Updating cryptography version.
    Signed-off-by: Morgan Nolan <morgan-nolan@ibm.com>

--- deployment/build.sh
+++ deployment/build.sh
@@ -141,8 +141,8 @@ python -V
 python3 -V
 docker version
 
-pip3 install 'cryptography==42.0.5'
-pip3 install 'pyopenssl==24.1.0'
+pip3 install 'cryptography==44.0.2'
+pip3 install 'pyopenssl==25.0.0'
 
 
 if [ ! -d "$BUILD_HOME" ]; then
